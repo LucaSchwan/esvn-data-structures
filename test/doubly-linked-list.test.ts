91f9f1aaa8bb319cf0c8a6ff81c9bc4e444a64fb
ehrenschwan <luca@ehrenschwan.com>
2022-01-04 19:46:16 +0100

    added test for the constructor of DoublyLinkedList

--- test/doubly-linked-list.test.ts
+++ test/doubly-linked-list.test.ts
@@ -7,4 +7,21 @@ describe('DoublyLinkedList', () => {
     expect(list.getFirst()).to.equal(null);
     expect(list.getLast()).to.equal(null);
   });
+
+  it('should be able to be initialised with a Node as first and last Node.', () => {
+    let element: DoublyLinkedNode<string> = {
+      element: 'test',
+      next: null,
+      prev: null,
+    };
+    let list = new DoublyLinkedList<string>(element);
+
+    let first = list.getFirst();
+    let last = list.getLast();
+
+    expect(first).to.not.equal(null);
+    expect(last).to.not.equal(null);
+    if (first) expect(first.element).to.equal('test');
+    if (last) expect(last.element).to.equal('test');
+  });
 });
